f4fa783e540ccbbcd97cde1bcd47a10c19565ffc
hikikomoriphoenix <dondon1987@yandex.com>
2018-06-19 17:43:15 +0800

    Plain unit tests. Trying some few hamcrest matchers

---
+++ app/src/test/java/marabillas/loremar/androidtesting/PlainUnitTests.java
@@ -0,0 +1,60 @@
+package marabillas.loremar.androidtesting;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.both;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.either;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+
+public class PlainUnitTests {
+    @Test
+    public void trueTest() {
+        assertThat(true, is(true));
+    }
+
+    @Test
+    public void withinRange() {
+        assertThat(0, both(greaterThanOrEqualTo(0)).and(lessThan(5)));
+    }
+
+    @Test
+    public void or() {
+        assertThat(3, either(is(2)).or(is(3)));
+    }
+
+    @Test
+    public void isEqualString() {
+        assertThat("red", is(equalTo("red")));
+        assertThat("blue", is(equalTo("blue")));
+    }
+
+    @Test
+    public void containsGivenText() {
+        assertThat("bahaghari", containsString("hari"));
+    }
+
+    @Test
+    public void oldAssert() {
+        int x = 12;
+        assert x > 10 : "x is not greater than 10";
+    }
+
+    @Test
+    public void objectIsNull() {
+        Object obj = null;
+        assertThat(obj, is(nullValue()));
+    }
+
+    @Test
+    public void objectIsNotNull() {
+        Object obj = new Object();
+        assertThat(obj, is(notNullValue()));
+    }
+}
